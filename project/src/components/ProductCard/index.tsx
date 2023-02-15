eafe9f74245feee82b5d1685b2626bc5d55fc9cc
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2023-02-15 15:43:10 -0300

    feature/project: create form register

--- project/src/components/ProductCard/index.tsx
+++ project/src/components/ProductCard/index.tsx
@@ -1,7 +1,11 @@
 import { Container } from "./style";
 
 const ProductCard = () => {
-  return <Container></Container>;
+  return <Container>
+      <form action="">
+        <input type="text" name="" id="" />
+      </form>
+  </Container>;
 };
 
 export default ProductCard;
