e1e78736892005475d781930b8c1f3d3f1222222
João Gonçalves <joao_i0807@outlook.com>
2023-02-27 21:21:04 -0300

    feat/project/initialFormYup

--- project/src/pages/index.ts
+++ project/src/pages/index.ts
@@ -1,5 +1,6 @@
 import { DetailAnnouncement } from "./DetailAnnouncement";
 import { Home } from "./Home";
 import { Profile } from "./Profile";
+import { Register } from "./Register";
 
-export { Home, DetailAnnouncement, Profile };
+export { Home, DetailAnnouncement, Profile, Register };
--- project/src/routes/index.routes.tsx
+++ project/src/routes/index.routes.tsx
@@ -1,13 +1,14 @@
-import { Home, DetailAnnouncement, Profile } from "../pages";
+import { Home, DetailAnnouncement, Profile, Register } from "../pages";
 import { Route, Routes } from 'react-router-dom'
 
 const RoutesMain = () => {
 
-  return( 
+  return ( 
     <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/announcement/' element={<DetailAnnouncement />}/>
         <Route path='/profile/' element={<Profile />}/>
+        <Route path='/signup/' element={<Register />}/>
           {/* <Route path='/' element={<Login/>}/> */}
           {/* <Route path='/register' element={<Register/>}/> */}
           {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
